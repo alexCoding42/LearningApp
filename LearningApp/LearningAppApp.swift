39a8c2bebf3e30129c341b168fc90eddf9046525
alexCoding42 <alexandre.cisse.ac@gmail.com>
2023-03-24 11:25:10 +0100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Alexandre Cisse on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
