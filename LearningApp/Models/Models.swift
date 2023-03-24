526da0679860df1973209975253ef7b4444293a7
alexCoding42 <alexandre.cisse.ac@gmail.com>
2023-03-24 13:51:01 +0100

    add assets, add data files, add models, parse json and data in content model

---
+++ LearningApp/Models/Models.swift
@@ -0,0 +1,51 @@
+//
+//  Models.swift
+//  LearningApp
+//
+//  Created by Alexandre Cisse on 24/03/2023.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+struct Content: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson: Decodable, Identifiable {
+    
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+}
+
+struct Test: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var question: [Question]
+}
+
+struct Question: Decodable, Identifiable {
+    
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
